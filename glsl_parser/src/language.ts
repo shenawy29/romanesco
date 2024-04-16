18e9c14485e456c6d92a9f6e55a480ffaab7d513
Karim Salama <karimsalama94@hotmail.com>
2024-04-16 04:06:12 +0200

    GLSL : added support for builtin macros and all reserved keywords

--- glsl_parser/src/language.ts
+++ glsl_parser/src/language.ts
@@ -3,11 +3,11 @@ import { styleTags, tags as t, Tag } from "@lezer/highlight"
 import { LRLanguage, indentNodeProp, foldNodeProp, foldInside, delimitedIndent } from "@codemirror/language"
 
 export const keywords = "for while do break continue if else return switch case default struct";
-export const reserved_keywords = "goto typedef enum class template using namespace extern this sizeof static volatile public long";
 export const glsl_keywords = "in out uniform sample flat layout";
 
 const style_tags: { [key: string]: Tag } = {
-  "Builtin": t.keyword,
+  "Builtin BuiltinMacro": t.keyword,
+  "Reserved": t.keyword,
   "PreprocDirectiveName": t.processingInstruction,
   "Identifier": t.variableName,
   "CallExpression/Identifier": t.function(t.variableName),
@@ -23,7 +23,7 @@ const style_tags: { [key: string]: Tag } = {
   "{ }": t.brace,
   "[ ]": t.squareBracket
 };
-style_tags[keywords] = style_tags[reserved_keywords] = style_tags[glsl_keywords] = t.keyword;
+style_tags[keywords] = style_tags[glsl_keywords] = t.keyword;
 
 export const GLSLLanguage = LRLanguage.define({
   name: "GLSL",
