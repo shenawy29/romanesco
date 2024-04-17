6854c8befcab2b5463d45f5baa28665c3463e688
Karim Salama <karimsalama94@hotmail.com>
2024-04-17 23:02:22 +0200

    added autocompletion for user-defined variables and functions locally

--- glsl_parser/src/autocomplete.ts
+++ glsl_parser/src/autocomplete.ts
@@ -1,29 +1,88 @@
 import { GLSLLanguage, keywords, glsl_keywords } from "./language"
+
 import { syntaxTree } from "@codemirror/language"
-import { SyntaxNode } from "@lezer/common"
+import { SyntaxNode, SyntaxNodeRef, IterMode, NodeWeakMap } from "@lezer/common"
 import { completeFromList, CompletionContext, CompletionResult, Completion } from "@codemirror/autocomplete"
 
-function local_completion(context: CompletionContext): CompletionResult | null {
-    let inner_node = syntaxTree(context.state).resolveInner(context.pos, -1);
-    //let inner = syntaxTree(context.state).resolveInner(context.pos, -1);
-    ////if (dontComplete.indexOf(inner.name) > -1) return null
-    let is_word = inner_node.name == "Identifier";
+// Just to be explicit about the type we are referencing by "Text"
+import { Text } from "@codemirror/state"
+
+// Helper functions for variable and function declarations. It would be nice to abstract them in a map like in the javascript example
+function IsVariableDeclaration(node: SyntaxNodeRef) { return node.name == "VariableDeclaration"; }
+function GetVariableNameNodeFromDeclaration(node: SyntaxNodeRef) { return node.node.getChild("Identifier"); }
+
+function IsFunctionDefinition(node: SyntaxNodeRef) { return node.name == "FunctionDeclaration" || node.name == "FunctionDefinition"; }
+function GetFunctionNameNodeFromDefinition(node: SyntaxNodeRef) { return node.node.getChild("FunctionHeader")?.getChild("Identifier"); }
+
+// Keep one "static" cache for now, but it would be nice to have one per extension instance
+const cache = new NodeWeakMap<readonly Completion[]>();
+
+function GetDefinitionsUntil(doc: Text, node: SyntaxNode, until: number) {
+    let cached = cache.get(node);
+    if (cached) return cached;
+
+    let definitions: Completion[] = [];
+
+    // Iterate through all the nodes in the block and gather all the definitions.
+    node.cursor(IterMode.IncludeAnonymous).iterate(current_node => {
+        // Stop iterating when we reach the limit
+        if (current_node.from >= until) return false;
+
+        // Variables
+        if (IsVariableDeclaration(current_node)) {
+            const variable_name_node = GetVariableNameNodeFromDeclaration(current_node);
+            if (variable_name_node)
+                definitions.push({ label: doc.sliceString(variable_name_node.from, variable_name_node.to), type: "variable" });
+        }
+        else
+            // Functions
+            if (IsFunctionDefinition(current_node)) {
+                const function_name_node = GetFunctionNameNodeFromDefinition(current_node);
+                if (function_name_node)
+                    definitions.push({ label: doc.sliceString(function_name_node.from, function_name_node.to), type: "function" });
+            }
+
+        // TODO : gather definitions for struts and macros
+    })
+
+    if (node.parent)
+        definitions = definitions.concat(GetDefinitionsUntil(doc, node.parent, node.from));
+
+    cache.set(node, definitions);
+
+    return definitions;
+}
+
+function LocalCompletion(context: CompletionContext): CompletionResult | null {
+    const inner_node = syntaxTree(context.state).resolveInner(context.pos, -1);
+
+    const is_word = inner_node.name == "Identifier";
     if (!is_word && !context.explicit) return null;
 
-    let options: Completion[] = []
-    console.log(context.state.doc.sliceString(inner_node.from, inner_node.to));
-    for (let node: SyntaxNode | null = inner_node; node; node = node.parent) {
-        //if (ScopeNodes.has(pos.name)) options = options.concat(getScope(context.state.doc, pos))
+    // SyntaxTree.resolveInner does not find the closest node to the position, but the innermost one containing it.
+    // If the user request for completions explicitly while the cursor is in the middle of whitespace, resolveInner
+    // will return the block containing the cursor...
+    const is_block = inner_node.name == "Block";
+
+    // ... this is why we are starting the scope scanning in a different way depending on the node type.
+    const block = is_block ? inner_node : inner_node.parent;
+
+    // Check if the block is valid : the parent can still be null
+    if (block) {
+        const definitions = GetDefinitionsUntil(context.state.doc, block, context.pos);
+        if (definitions.length != 0)
+            return {
+                options: definitions,
+
+                // TODO : figure out what the following "from" value does. (cargo-culted for now)
+                from: is_word ? inner_node.from : context.pos,
+
+                // Let's just copy this from the javascript example and assume it matches all identifiers
+                validFor: /^[\w$\xa1-\uffff][\w$\d\xa1-\uffff]*$/
+            };
     }
 
-    options.push({ label: "ggg" });
-
-    return {
-        options,
-        from: is_word ? inner_node.from : context.pos,
-        //  validFor: Identifier
-    }
-    return { options, from: 1 };
+    return null;
 }
 
 // All of the language's keywords
@@ -60,7 +119,7 @@ export const autocomplete_extensions = [
         autocomplete: completeFromList(keyword_completions.concat(primitive_type_completions).concat(constants).concat(builtin_functions))
     }),
 
-    //GLSLLanguage.data.of({
-    //  autocomplete: local_completion
-    //})
+    GLSLLanguage.data.of({
+        autocomplete: LocalCompletion
+    })
 ];
